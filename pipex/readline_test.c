ea26965fba4b8f2aa6a0b8b005f607e19553ddc9
Omar Zini <omarzini82@gmail.com>
2024-05-14 19:31:51 +0200

    Modifications to the parse bit of Alejandro added

--- pipex/readline_test.c
+++ pipex/readline_test.c
@@ -6,7 +6,7 @@
 /*   By: ozini <ozini@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 11:31:33 by ozini             #+#    #+#             */
-/*   Updated: 2024/05/14 17:10:39 by ozini            ###   ########.fr       */
+/*   Updated: 2024/05/14 18:49:28 by ozini            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	main(int argc, char **argv, char **env)
 
 	(void)argv;
 	(void)argc;
-	atexit(ft_leaks);
+	//atexit(ft_leaks);
 	exit_status = 0;
 	getcwd(cwd, sizeof(cwd));
 	env_list = create_env_list(env);
@@ -46,22 +46,17 @@ int	main(int argc, char **argv, char **env)
 		if (ft_strlen(input))
 		{
 			add_history(input);
-			//if(ft_check_syntax(input))
-				//exit_status = 258;
-			//else
-			//{
-			processes = parser(input, env_list);
-			exit_status = run_prompt(processes, &env_list, &export_list);
-			//}
-/* 			exit_status = check_ft(input);
-			if (!exit_status)
+			if(ft_check_syntax(input))
+				exit_status = 258;
+			else
 			{
-				processes = parser(input, env_list);
-				exit_status = pipex(processes, &env_list, &export_list);
-			} */
+			processes = parser(input, env_list, exit_status);
+			exit_status = run_prompt(processes, &env_list, &export_list);
+			}
 			free(input);
 			clean_up_processes_list(processes);
-			printf("exit_status: %d\n", exit_status);
+			processes = NULL;
+			//printf("exit_status: %d\n", exit_status);
 		}
 	}
 	while (env_list != NULL)
