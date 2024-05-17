1f40f5b8618ccff53cc61ae0329b666c9c538ab2
Omar Zini <omarzini82@gmail.com>
2024-05-17 22:32:21 +0200

    improving heredoc signals

--- pipex/execution_heredoc_file.c
+++ pipex/execution_heredoc_file.c
@@ -1,16 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils_heredoc_file.c                               :+:      :+:    :+:   */
+/*   execution_heredoc_file.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ozini <ozini@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 10:43:19 by ozini             #+#    #+#             */
-/*   Updated: 2024/05/14 15:03:01 by ozini            ###   ########.fr       */
+/*   Updated: 2024/05/17 22:31:39 by ozini            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
 
 char	*create_heredoc_filename(int here_doc_process_counter)
 {
@@ -51,27 +54,47 @@ static	int	delimiter_vs_input(char *delimiter, char *buffer)
 		return (0);
 }
 
+
+
+void signal_handler(int signum) {
+    (void)signum;
+	printf("\n");
+	//write(STDOUT_FILENO, "\nReceived signal\n", 16);
+}
+
 int	create_heredoc_file(char *delimiter, char *here_doc_filename)
 {
-	int		fd;
-	char	buffer[BUFFER_SIZE];
-	ssize_t	bytes_read;
+    struct sigaction sa;
+    int		fd;
+    char	buffer[BUFFER_SIZE];
+    ssize_t	bytes_read;
 
-	bytes_read = 1;
-	fd = open(here_doc_filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	if (fd == -1)
-		return (0);
-	while (bytes_read > 0)
-	{
-		ft_putstr_fd("> ", STDOUT_FILENO);
-		ft_memset(buffer, 0, sizeof(buffer));
-		bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-			return (heredoc_read_error(fd, here_doc_filename));
-		if (delimiter_vs_input(delimiter, buffer))
-			break ;
-		ft_putstr_fd(buffer, fd);
-	}
-	close(fd);
-	return (1);
+    sa.sa_handler = signal_handler;
+    sa.sa_flags = 0;
+
+    sigaction(SIGINT, &sa, NULL);
+
+    bytes_read = 1;
+    fd = open(here_doc_filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if (fd == -1)
+        return (0);
+    while (bytes_read > 0)
+    {
+        ft_putstr_fd("> ", STDOUT_FILENO);
+        ft_memset(buffer, 0, sizeof(buffer));
+        bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        if (bytes_read == -1)
+        {
+			if (errno == EINTR) {
+                // Handle the interruption here
+                continue;
+            }
+            return (heredoc_read_error(fd, here_doc_filename));
+        }
+        if (delimiter_vs_input(delimiter, buffer))
+            break ;
+        ft_putstr_fd(buffer, fd);
+    }
+    close(fd);
+    return (1);
 }
