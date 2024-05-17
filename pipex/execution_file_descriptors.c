a9135428809f079931eb8c2893de5cfa011aaa7e
Omar Zini <omarzini82@gmail.com>
2024-05-17 15:03:00 +0200

    Handled case for outfile without permissions
    Added the function fd_error as a result of splitting
    handle_file_descriptors into two.

--- pipex/execution_file_descriptors.c
+++ pipex/execution_file_descriptors.c
@@ -6,7 +6,7 @@
 /*   By: ozini <ozini@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 12:51:26 by ozini             #+#    #+#             */
-/*   Updated: 2024/05/16 17:09:04 by ozini            ###   ########.fr       */
+/*   Updated: 2024/05/17 14:53:47 by ozini            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,23 @@ static void	handle_fd_outfile_append(t_list *list, t_list *redirections)
 			O_WRONLY | O_CREAT | O_APPEND, 0777);
 }
 
+static	int	fd_error(t_list *list, t_list *redirections)
+{
+	if (((t_process *)list->content)->fd_infile == -1
+		|| ((t_process *)list->content)->fd_outfile == -1)
+	{
+		if (((t_process *)list->content)->fd_outfile != -1)
+			close(((t_process *)list->content)->fd_outfile);
+		if ((((t_process *)list->content)->heredoc))
+			unlink((((t_process *)list->content)->heredoc));
+		bash_error_message(strerror(errno),
+			((t_redir *)redirections->content)->name);
+		return (1);
+	}
+	else
+		return (0);
+}
+
 int	handle_file_descriptors(t_list *list)
 {
 	t_list	*redirections;
@@ -59,16 +76,8 @@ int	handle_file_descriptors(t_list *list)
 			handle_fd_outfile_truncate(list, redirections);
 		else if (((t_redir *)redirections->content)->value == APPEND)
 			handle_fd_outfile_append(list, redirections);
-		if (((t_process *)list->content)->fd_infile == -1)
-		{
-			if (((t_process *)list->content)->fd_outfile)
-				close(((t_process *)list->content)->fd_outfile);
-			if ((((t_process *)list->content)->heredoc))
-				unlink((((t_process *)list->content)->heredoc));
-			bash_error_message(strerror(errno),
-				((t_redir *)redirections->content)->name);
+		if (fd_error(list, redirections))
 			return (-1);
-		}
 		redirections = redirections->next;
 	}
 	return (0);
